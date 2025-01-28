f216590c809b9cd3c920818545f4c1829e0363ec
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 22:35:57 +0600

    server main commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", ""],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
